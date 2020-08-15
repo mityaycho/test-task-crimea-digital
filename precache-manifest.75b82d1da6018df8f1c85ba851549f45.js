99f7f73bbd5e1d07d7b3e9583e2bb837370cd6e5
mityaycho <mityaycho@gmail.com>
2020-08-15 21:56:22 +0300

    Updates

---
+++ precache-manifest.75b82d1da6018df8f1c85ba851549f45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e001a1b051cff786a94a71581c38c8",
+    "url": "/test-task-crimea-digital/index.html"
+  },
+  {
+    "revision": "359f4f1d5e9469ee95ec",
+    "url": "/test-task-crimea-digital/static/css/main.0089b69b.chunk.css"
+  },
+  {
+    "revision": "330736fdfd33442d3f46",
+    "url": "/test-task-crimea-digital/static/js/2.73da2f8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-task-crimea-digital/static/js/2.73da2f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359f4f1d5e9469ee95ec",
+    "url": "/test-task-crimea-digital/static/js/main.109a4abc.chunk.js"
+  },
+  {
+    "revision": "cbdfcffc08acd0d41cd7",
+    "url": "/test-task-crimea-digital/static/js/runtime-main.8b0d36fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-task-crimea-digital/static/media/logo.5d5d9eef.svg"
+  }
+]);
